0eb93474f3782e923402980cfc067e02021803f4
Jehaann-Manzanares <jehaann07carlos@gmail.com>
2020-07-15 15:39:34 -0600

    feature (splashActivity ): agregando splash activity

--- app/src/main/java/com/example/geotriviaapp/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/example/geotriviaapp/ui/slideshow/SlideshowViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Contactame"
     }
     val text: LiveData<String> = _text
 }
